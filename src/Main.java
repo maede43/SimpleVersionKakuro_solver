019cbbdf81a61a631a7e064680afa476a4adc497
Maede <maede.nadehi@yahoo.com>
2021-04-21 14:37:45 +0430

    get a 2D array as puzzle board from user

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+/*
+    *
+    * Title:  Kakuro Solver as CSP problem
+    * Author: Maedeh Nadehi
+    * Date:   April 2021
+    *
+*/
+import java.util.Scanner;
+
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int columns = sc.nextInt();
+        int rows = sc.nextInt();
+        int board[][] = new int[rows][columns];
+        // get puzzle board
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                board[i][j] = sc.nextInt();
+            }
+        }
+
+    }
+}
